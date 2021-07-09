53758421d85f2b9ab26cf6570dfd9be85b90a923
Matija Nalis <mnalis-git@voyager.hr>
2021-07-09 14:42:09 +0200

    change contact email, as not to bother upstream with this version bugs (fix conflicts)

--- app/src/main/java/de/westnordost/streetcomplete/ApplicationModule.kt
+++ app/src/main/java/de/westnordost/streetcomplete/ApplicationModule.kt
@@ -34,7 +34,7 @@ import javax.inject.Singleton
     @Provides @Singleton fun exceptionHandler(ctx: Context): CrashReportExceptionHandler =
         CrashReportExceptionHandler(
             ctx,
-            "streetcomplete_errors@westnordost.de",
+            "mnalis-streetcomplete@voyager.hr",
             "crashreport.txt"
         )
 }
