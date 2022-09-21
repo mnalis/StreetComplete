c98815d9645503cd73c649cca1d6a78a5e282931
Matija Nalis <mnalis-git@voyager.hr>
2022-09-21 02:00:58 +0200

    Revert "add debug" (orientation-mode-pref)
    This reverts commit 6e427b4244f095530e7109ff6b61ac582aededab.

--- app/src/main/java/de/westnordost/streetcomplete/screens/main/map/LocationAwareMapFragment.kt
+++ app/src/main/java/de/westnordost/streetcomplete/screens/main/map/LocationAwareMapFragment.kt
@@ -206,11 +206,8 @@ open class LocationAwareMapFragment : MapFragment() {
         updateCameraPosition(600) {
             if (isNavigationMode) {
                 val isCompassDirection = NavigationOrientationUpdater.isCompassDirection
-                val bearing1 = getTrackBearing(tracks.last())
-                val bearing2 = locationMapComponent?.rotation // /mn/ trying to track compass
-                Log.d("centerCurrentPosition", "getTrackBearing is ${bearing1} and locationMapComponent.rotation is ${bearing2}")
-                val bearing : Float? = if (isCompassDirection) locationMapComponent?.rotation?.toFloat() else getTrackBearing(tracks.last())
-                Log.d("centerCurrentPosition", "isCompassDirection = ${isCompassDirection}, newbearing=${bearing}")
+                val bearing = if (isCompassDirection) locationMapComponent?.rotation else getTrackBearing(tracks.last())
+                Log.d("centerCurrentPosition", "isCompassDirection = ${isCompassDirection}, bearing=${bearing}")
                 if (bearing != null) {
                     rotation = -(bearing * PI / 180.0).toFloat()
                     /* move center position down a bit, so there is more space in front of than
