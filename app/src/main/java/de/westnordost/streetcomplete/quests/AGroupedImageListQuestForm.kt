5465bb027ccc3191b837f0f2e0fecb9f762945f1
Matija Nalis <mnalis-git@voyager.hr>
2022-11-03 02:45:27 +0100

    mnalis: try fid compile

--- app/src/main/java/de/westnordost/streetcomplete/quests/AGroupedImageListQuestForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/AGroupedImageListQuestForm.kt
@@ -95,7 +95,7 @@ abstract class AGroupedImageListQuestForm<I, T> : AbstractOsmQuestForm<T>() {
     }
 
     private fun getInitialItems(): List<GroupableDisplayItem<I>> =
-        favs.get().mostCommonWithin(6, historyCount = 50, first = 1).padWith(topItems).toList().map { Item2(it.value, it.image, it.title, it.description) }
+        favs.get().mostCommonWithin(6, historyCount = 50, first = 1).map { Item2(it.value, it.image, it.title, it.description) }.padWith(topItems).toList()
 
     override fun onClickOk() {
         val item = selectedItem!!
