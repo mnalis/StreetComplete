808fb91dfa5bed3f116ff2d7a244f5d939ff2057
Matija Nalis <mnalis-git@voyager.hr>
2022-12-08 03:24:46 +0100

    /mn/ ne pitaj za cycleway na driveway, parking_aisle itd.

--- app/src/main/java/de/westnordost/streetcomplete/quests/cycleway/AddCycleway.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/cycleway/AddCycleway.kt
@@ -190,7 +190,7 @@ private val roadsFilter by lazy { """
 private val untaggedRoadsFilter by lazy { """
     ways with (
         highway ~ primary|primary_link|secondary|secondary_link|tertiary|tertiary_link|unclassified
-        or highway ~ residential|service|pedestrian
+        or (highway ~ residential|service|pedestrian and !service)
       )
       and !cycleway
       and !cycleway:left
