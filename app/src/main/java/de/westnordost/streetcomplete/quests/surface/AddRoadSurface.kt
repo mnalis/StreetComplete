24a8a13c1b7c9ca995b1ff5fdc13da259deedfb7
Matija Nalis <mnalis-git@voyager.hr>
2024-02-06 01:47:53 +0100

    restore part removed for previous debug

--- app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddRoadSurface.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/surface/AddRoadSurface.kt
@@ -26,6 +26,8 @@ class AddRoadSurface : OsmFilterQuestType<SurfaceAndNote>() {
         )
         and (
           !surface
+          or surface ~ ${ANYTHING_UNPAVED.joinToString("|")} and surface older today -6 years
+          or surface older today -12 years
           or (
             surface ~ paved|unpaved|${INVALID_SURFACES.joinToString("|")}
             and !surface:note
