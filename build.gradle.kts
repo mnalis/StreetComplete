5a93cb06a82485298db2c08cf87fee89d5194b1b
Matija Nalis <mnalis-git@voyager.hr>
2025-01-03 20:56:57 +0100

    compile error

--- build.gradle.kts
+++ build.gradle.kts
@@ -136,7 +136,7 @@ tasks.withType<Test> {
     testLogging {
         events("passed", "skipped", "failed")
         //showStandardStreams = true // Show output from println statements
-        exceptionFormat = TestExceptionFormat.FULL // Show full stack trace for exceptions
+        exceptionFormat = "full" // Show full stack trace for exceptions
         // Optionally, you can also set:
         showExceptions = true // Show exceptions for failed tests
         showCauses = true // Show causes for failed tests
