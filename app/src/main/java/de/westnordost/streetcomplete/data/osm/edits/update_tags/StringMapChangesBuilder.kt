8f561009ddf8635cb86222ea49f54f2b8eac61b0
Matija Nalis <mnalis-git@voyager.hr>
2025-01-04 00:15:31 +0100

    avoid infinite recursion see e.g. https://github.com/mnalis/StreetComplete/actions/runs/12605024760/job/35133015071
            at de.westnordost.streetcomplete.osm.ResurveyUtilsKt.removeCheckDatesForKey(ResurveyUtils.kt:74)
            at de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder.remove(StringMapChangesBuilder.kt:20)
            at de.westnordost.streetcomplete.osm.ResurveyUtilsKt.removeCheckDatesForKey(ResurveyUtils.kt:74)
            at de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder.remove(StringMapChangesBuilder.kt:20)
            at de.westnordost.streetcomplete.osm.ResurveyUtilsKt.removeCheckDatesForKey(ResurveyUtils.kt:74)

--- app/src/main/java/de/westnordost/streetcomplete/data/osm/edits/update_tags/StringMapChangesBuilder.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/osm/edits/update_tags/StringMapChangesBuilder.kt
@@ -17,7 +17,8 @@ class StringMapChangesBuilder(private val source: Map<String, String>) : Map<Str
     /** Remove the given key (and related keys with metadata) from the map */
     fun remove(key: String) {
         removeOne(key)
-        removeCheckDatesForKey(key) // FIXME: it seems to break test that modify existing tag xxx and thus create check_date:xxx
+        // removeCheckDatesForKey(key)     // FIXME: this one would be better as it remove other check_date keys too, but triggers infinite recursion, so should be done in other way
+        removeOne("check_date:" + key) // FIXME: it seems to break test that modify existing tag xxx and thus create check_date:xxx
         removeOne("source:" + key)
     }
 
