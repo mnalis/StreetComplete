8f516eba39d72cca1dc88219aeabfa749e20f618
Matija Nalis <mnalis-git@voyager.hr>
2025-01-03 20:53:16 +0100

    try to get logging of passed tests to work

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,5 +135,11 @@ tasks.register("updateStreetCompleteData") {
 tasks.withType<Test> {
     testLogging {
         events("passed", "skipped", "failed")
+        //showStandardStreams = true // Show output from println statements
+        exceptionFormat = TestExceptionFormat.FULL // Show full stack trace for exceptions
+        // Optionally, you can also set:
+        showExceptions = true // Show exceptions for failed tests
+        showCauses = true // Show causes for failed tests
+        showStackTraces = true // Show stack traces for failed tests
     }
 }
