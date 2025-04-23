b82d500f6f98a555ccad81a300655a50dfae71ea
Matija Nalis <mnalis-git@voyager.hr>
2025-04-23 15:14:54 +0200

    start with 16x Web CSS 1.0 colors

--- app/src/main/java/de/westnordost/streetcomplete/quests/building_colour/BuildingColour.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/building_colour/BuildingColour.kt
@@ -4,6 +4,26 @@ import de.westnordost.streetcomplete.view.image_select.OsmColour
 
 enum class BuildingColour(override val osmValue: String, override val androidValue: String?) :
     OsmColour {
+
+    // start with 16x Web CSS 1.0 colors - https://www.w3.org/TR/css-color-3/#html4
+    // see https://github.com/Helium314/SCEE/issues/633#issuecomment-2603329012
+    BLACK("black", "#000000"),
+    SILVER("silver", "#c0c0c0"),
+    GRAY("gray", "#808080"),
+    WHITE("white", "#ffffff"),
+    MAROON("maroon", "#800000"),
+    RED("red", "#ff0000"),
+    PURPLE("purple", "#800080"),
+    FUCHSIA("fuchsia", "#ff00ff"),
+    GREEN("green", "#008000"),
+    LIME("lime", "#00ff00"),
+    OLIVE("olive", "#808000"),
+    YELLOW("yellow", "#ffff00"),
+    NAVY("navy", "#000080"),
+    BLUE("blue", "#0000ff"),
+    TEAL("teal", "#008080"),
+    AQUA("aqua", "#00ffff "),
+
     // Top used building colours
     WHITE("white", "#ffffff"),
     GREY80("#cccccc", null),
